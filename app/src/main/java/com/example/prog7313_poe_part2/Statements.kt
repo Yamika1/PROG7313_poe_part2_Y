0d853822e793123017a0b09604ff9fa3c374c17e
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-04-26 14:15:51 +0200

    commit 1

--- app/src/main/java/com/example/prog7313_poe_part2/Statements.kt
+++ app/src/main/java/com/example/prog7313_poe_part2/Statements.kt
@@ -3,37 +3,46 @@ package com.example.prog7313_poe_part2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Budget_Entry : AppCompatActivity() {
+class Statements : AppCompatActivity() {
 
-
+    private lateinit var search_button: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_budget_entry)
+        setContentView(R.layout.statements)
 
 
-        val getToActivity = findViewById< ImageButton>(R.id.backbutton)
-        getToActivity.setOnClickListener{
+        search_button = findViewById(R.id.search_button)
+        search_button.setOnClickListener() {
+
+
+        }
+        val getToActivity = findViewById<ImageButton>(R.id.backbutton)
+        getToActivity.setOnClickListener {
             val intent = Intent(getApplicationContext(), MainActivity::class.java)
             startActivity(intent)
         }
 
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
 
-//        imgButton.setOnClickListener {
-//
-//        }
