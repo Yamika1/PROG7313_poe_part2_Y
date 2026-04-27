efef6ed2803a81d2411a3ba0682cfa5fe2078811
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-04-27 10:17:36 +0200

    commit 2

--- app/src/main/java/com/example/prog7313_poe_part2/MainActivity.kt
+++ app/src/main/java/com/example/prog7313_poe_part2/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.prog7313_poe_part2
 
-import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
 import android.widget.Button
@@ -15,33 +14,29 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
-
+private lateinit var editTextText: EditText
+    private lateinit var editTextText2: EditText
+    private lateinit var button: Button
+    private lateinit var button2: Button
+   // private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+//        db = AppDatabase.getDatabase(this)
 
         textView = findViewById(R.id.textView)
+        editTextText = findViewById(R.id.editTextText)
+        editTextText2 = findViewById(R.id.editTextText2)
+        button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
         textView.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
-        val emailInput = findViewById<EditText>(R.id.editTextText)
-        val passwordInput = findViewById<EditText>(R.id.editTextText2)
-        val buttonMessage = findViewById<Button>(R.id.button)
 
-
-        buttonMessage.setOnClickListener {
-            val email = emailInput.text.toString()
-            val password = passwordInput.text.toString()
-
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                Toast.makeText(this, "Hello $email :)", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Please enter the email and password", Toast.LENGTH_SHORT).show()
-
-
-
+        button.setOnClickListener {
+            login()
+        }
 
 
                 ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -55,6 +50,19 @@ class MainActivity : AppCompatActivity() {
                     insets
                 }
             }
+    private fun login(){
+val username = editTextText.text.toString().trim()
+        val password = editTextText2.text.toString().trim()
+
+        if(username.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Enter the username and password", Toast.LENGTH_SHORT).show()
+        }else{
+                    if (username.isNotEmpty() && password.isNotEmpty()) {
+                        Toast.makeText(this, "Hello $username :)", Toast.LENGTH_SHORT).show()
+                    }
+
+
+        }
+
         }
     }
-}
--- app/src/main/java/com/example/prog7313_poe_part2/Statements.kt
+++ app/src/main/java/com/example/prog7313_poe_part2/Statements.kt
@@ -35,8 +35,6 @@ class Statements : AppCompatActivity() {
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
 
 
-
-
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
                 val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -44,5 +42,4 @@ class Statements : AppCompatActivity() {
             }
         }
     }
-}
 
